f9c7eb426e587f3a7d34d8c7b1b573856567b8a0
Zach J Murphy <78761892+zachjmurphy@users.noreply.github.com>
2021-08-29 18:08:00 -0400

    Update index.js

--- index.js
+++ index.js
@@ -3,8 +3,18 @@ const app = express();
 
 app.use(express.static('public'));
 
+// Set up Rate Limiter: Maximum of five requests per minute
+var RateLimit = require('express-rate-limit');
+var limiter = new RateLimit({
+  windowMs: 1*60*1000, // 1 minute
+  max: 5
+});
+
+// Apply Rate Limiter to all Requests
+app.use(limiter);
+
 app.get('/', (req, res) => {
     res.sendFile(process.cwd() + '/public/index.html')
 });
 
-app.listen(3000, () => console.log('server started'));
+app.listen(3000, () => console.log('Server has started...'));
