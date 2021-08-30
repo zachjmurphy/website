d56076dd6c052c6712aa39799c4b5c1c053897fe
Zach J Murphy <78761892+zachjmurphy@users.noreply.github.com>
2021-08-29 20:15:10 -0400

    Added New Pages

--- index.js
+++ index.js
@@ -13,8 +13,25 @@ var limiter = new RateLimit({
 // Apply Rate Limiter to all Requests
 app.use(limiter);
 
+// Home Page
 app.get('/', (req, res) => {
-    res.sendFile(process.cwd() + '/public/index.html')
+    res.sendFile(process.cwd() + '/views/index.html')
 });
 
+// About Page
+app.get('/about', (req, res) => {
+  res.sendFile(process.cwd() + '/views/about.html')
+});
+
+// Projects Page
+app.get('/projects', (req, res) => {
+  res.sendFile(process.cwd() + '/views/projects.html')
+});
+
+// Gallery Page
+app.get('/gallery', (req, res) => {
+  res.sendFile(process.cwd() + '/views/gallery.html')
+});
+
+// Listen & Start Server
 app.listen(3000, () => console.log('Server has started...'));
